60fc17b9919d52cdcacedb21b7f86529c96383e6
Berke Aras <info@berkearas.de>
2022-01-14 21:09:12 +0100

    📈 Add Google Analytics (WIP)

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, {useEffect} from 'react';
 import './styles/global.scss';
 import Header from './components/header';
 import Footer from './components/footer';
@@ -6,11 +7,18 @@ import {
 	Routes,
 	Route
 } from "react-router-dom";
+import ReactGA from "react-ga4";
 
 // Views
 import Home from './views/Home';
 
 function App() {
+
+	useEffect(() => {
+		ReactGA.initialize("G-D7E5P3TKS7");
+		ReactGA.send("pageview");
+	})
+
 	return (
 		<BrowserRouter>
 			<div className="App">
