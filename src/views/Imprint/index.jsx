919b0535dc2980b8d01e40cd0426f23ea6c9702a
Berke Aras <36629890+BerkeAras@users.noreply.github.com>
2022-05-31 18:42:41 +0200

    Update Imprint

--- src/views/Imprint/index.jsx
+++ src/views/Imprint/index.jsx
@@ -28,6 +28,13 @@ export default function Imprint() {
                 Telefon: 0176 30155690<br />
                 E-Mail: info@berkearas.de<br />
                 <br />
+		<b>Umsatzsteuer-ID</b><br />
+		Umsatzsteuer-Identifikationsnummer gemäß § 27 a Umsatzsteuergesetz:<br />
+		DE352511085<br />
+		<br />
+		<b>EU-Streitschlichtung</b><br />
+		Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: <a href="https://ec.europa.eu/consumers/odr/" target="_blank">https://ec.europa.eu/consumers/odr/</a>. Unsere E-Mail-Adresse finden Sie oben im Impressum.
+		<br />
                 <b>Haftung für Inhalte</b><br />
                 Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.<br />
                 <br />
